366230b406b6518b8cfd4d755a0f8fb248815a29
Luke Chen <luke.chen.2010@gmail.com>
2020-05-04 21:09:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,14 +4,12 @@ def line(customers)
   if(customers.size == 0)
     puts "The line is currently empty."
   else
-  
+    line_string = "The line is currently:"
+    
+    customers.each do |customer|
+      line_string = line_string + " #{customers.index(customer) + 1}. #{customer}"
+    end
+    
+    puts line_string
   end
-
-  line_string = "The line is currently:"
-  
-  customers.each do |customer|
-    line_string = line_string + " #{customers.index(customer) + 1}. #{customer}"
-  end
-  
-  puts line_string
 end
